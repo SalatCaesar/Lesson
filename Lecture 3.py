28f0094a76ad1cf1c9272666c1d8b680449f0249
SalatCaesar <strutynskiy.ao@gmail.com>
2025-10-23 19:18:38 +0300

    Лекция 5

---
+++ Lecture 3.py
@@ -0,0 +1,8 @@
+o = 3
+print(id(o))
+l = [3]
+print(id(l))
+print(id(l[0]))
+l.append(1)
+print(l)
+print(id(l))
--- Lecture 4.py
+++ Lecture 4.py
@@ -1,25 +1,5 @@
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-str = '1234567890'
-tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
-print(list)
-print(str)
-print(tuple)
-for i in list:
-    print(i + 1)
-print()
-for i in str:
-    print(int(i)+1)
-print()
-for i in tuple:
-    print(tuple[i-1::2])
-print()
-d = 0
-for i in list:
-    if list[i-1] % 2 == 0:
-     d += list[i-1]
-print(d)
-print()
+for i in range(4):  # stop=4
+     print(i)
 
-a = 2
-z = 2
-print(a is z)
+
+print(list(range(2,4,2)))
