288e1a443f754f26ec2df8554e391b533fc866ae
SalatCaesar <strutynskiy.ao@gmail.com>
2025-11-16 18:53:41 +0300

    Лекция 5

--- Lecture 3.py
+++
@@ -1,8 +0,0 @@
-o = 3
-print(id(o))
-l = [3]
-print(id(l))
-print(id(l[0]))
-l.append(1)
-print(l)
-print(id(l))
--- Lecture 4.py
+++
@@ -1,5 +0,0 @@
-for i in range(4):  # stop=4
-     print(i)
-
-
-print(list(range(2,4,2)))
