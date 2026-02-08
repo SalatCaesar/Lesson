89008802d7706bced97100d60904804d8f8b24d8
ao.strutynskiy <ao.strutynskiy@tensor.ru>
2026-02-08 23:00:25 +0300

    New

--- Lesson_11/L_5.py
+++ Lesson_11/L_5.py
@@ -10,7 +10,6 @@ try:
     sleep(1)
     assert browser.current_url == sbis_site, 'Неверно открыт сайт'
     toolbar = browser.find_element(By.CSS_SELECTOR, '.sbisru-Header__button')
-
     toolbar.click()
 finally:
     browser.quit()
